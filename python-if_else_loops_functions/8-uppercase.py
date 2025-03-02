f50b126ac85d89d5a89cd166f6d378b0c50ac472
Alain Pacifique UWENAYO <66881609+uwenayoallain@users.noreply.github.com>
2025-03-02 22:48:44 +0000

    Fix Task 8: Function to print string in uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
+    result = ""
     for char in str:
-        print(f"{chr(ord(char) - 32) if 'a' <= char <= 'z' else char}", end="")
-    print()
+        if ord('a') <= ord(char) <= ord('z'):
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+    print("{}".format(result))
